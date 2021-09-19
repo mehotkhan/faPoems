10433030e47d5d34b4eefdf04c99ebc0314ab7bd
Ali Zemani <mehot1@gmail.com>
2021-09-19 13:01:42 +0430

    V1

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,18 +1,17 @@
 module.exports = {
-    lang: 'fa-IR',
-    title: 'کمی شعر بخوانیم',
-    description: 'شعرهای کهن فارسی را راحت تر بخوانیم و به اشتراک بگذاریم .',
-  
-    themeConfig: {
-      navbar: [
-        // NavbarItem
-        {
-          text: 'درباره اینجا',
-          link: '/about-here/',
-        },
-        // NavbarGroup
-     
-      ],
-      // logo: 'https://vuejs.org/images/logo.png',
-    },
-  }
+  lang: "fa-IR",
+  title: "کمی شعر بخوانیم",
+  description: "شعرهای کهن فارسی را راحت تر بخوانیم و به اشتراک بگذاریم .",
+  base: "/faPoems/",
+  themeConfig: {
+    navbar: [
+      // NavbarItem
+      {
+        text: "درباره اینجا",
+        link: "/about-here/",
+      },
+      // NavbarGroup
+    ],
+    // logo: 'https://vuejs.org/images/logo.png',
+  },
+};
